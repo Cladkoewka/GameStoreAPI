06eeea4cd792a1343fef36a4b49136908023e9a5
Cladkoewka <codewarriorms@gmail.com>
2024-08-11 16:32:55 +0300

    CRUD operations with db

---
+++ Dtos/GameSummaryDto.cs
@@ -0,0 +1,10 @@
+﻿namespace GameStore.API.Dtos
+{
+    public record class GameSummaryDto(
+        int Id,
+        string Name,
+        string GenreId,
+        decimal Price,
+        DateOnly ReleaseDate
+        );
+}
